880b024d4e35cb28a199a9dc91ed2788d2333d42
yijaejun <yijaejun81@gmail.com>
2017-12-24 12:24:03 +0900

    hello react

--- MEMO.md
+++ MEMO.md
@@ -2,8 +2,22 @@ npm install;
 npm start
 localhost:8080
 
+npm install로 설치된 각 요소의 버전이 업그레이드가 되어야 하는지 확인하자.
 
-TODO TEST
+
+
+
+React vs. ReactDOM
+
+
+
+
+
+
+
+
+
+<h3>TODO TEST</h3>
 1. 정적 홈페이지이지만 이러한 기술을 통해서 만들고 개시를 헤보자
 2.
 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
+// Create a new Component This component should produce
+// Some HTML
+// Take this component's generated HTML and put it on the page (in the DOM).
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+const App = () => {
+	return <div>Hello React!</div>;
+};
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// <App /> 이것이 클래스가 아니라 인스턴스화 하여 전달하는 것이다
+ReactDOM.render(<App />, document.querySelector('.container'));
